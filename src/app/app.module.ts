d5c32603eac1af86b626f82838de803fad627de5
Vaishnavi Allampallewar <vaishnavi_a@persistent.co.in>
2019-06-12 18:42:36 +0530

    employee-direstory project upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeDirectoryService } from './employee-directory.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponent   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [EmployeeDirectoryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
